ce21a7e0a33734cabe54607b9ed86dbc4824c047
Prerith <prerithudjain@gmail.com>
2025-07-26 18:46:21 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ this makes sure relative paths work on static hosts like Render
 })
